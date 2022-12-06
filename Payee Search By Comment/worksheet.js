3596e613c2286f746a6d78ecb83b8d88d6233401
Chris Benjamin <67335579+13cbenjamin@users.noreply.github.com>
2022-12-06 12:48:37 -0800

    Create worksheet.js

---
+++ Payee Search By Comment/worksheet.js
@@ -0,0 +1,5 @@
+$(document).on("keypress", "input", function(e){
+        if(e.which == 13){
+            WCgoToApp('CaseWorksheet.aspx?xwin=&amp;wstype=59&amp;standalone=1&amp;cid=-8888888');
+        }
+    });
